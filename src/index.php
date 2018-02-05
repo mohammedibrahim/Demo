b8bd3ec21b9f2058bfcfaf49d149a0029ed7bf64
Mohamed Ibrahim <m@mibrah.im>
2018-02-05 19:51:45 +0200

    Saving up

---
+++ src/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo(file_get_contents('http://192.168.2.9/Demo/'));
--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -3,7 +3,7 @@
 /**
  * @coversDefaultClass \Dracony\Demo\Hello
  */
-class HelloTest extends \PHPUnit_Framework_TestCase
+class HelloTest extends \PHPUnit\Framework\TestCase
 {
     protected $hello;
     
@@ -18,5 +18,12 @@ class HelloTest extends \PHPUnit_Framework_TestCase
     public function testWorld()
     {
         $this->assertSame('world', $this->hello->world());
+    }
+
+    public function testAPI()
+    {
+        $response = (file_get_contents('http://localhost/Demo/'));
+
+        $this->assertSame('Hello from the other side.', $response);
     }
 }
